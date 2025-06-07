a5199e28341ec98fa2a1310821d684168681bafd
ArekSkiba <arek.skiba77@gmail.com>
2025-06-07 19:27:05 +0200

    CH3 L4 Maze random generator

--- maze.py
+++ maze.py
@@ -12,7 +12,10 @@ class Maze:
             cell_size_x,
             cell_size_y,
             win=None,
+            seed=None
         ):
+        if seed is not None:
+            random.seed(seed)
         self.__x1 = x1
         self.__y1 = y1
         self.__num_rows = num_rows
@@ -23,6 +26,8 @@ class Maze:
 
         self.__cells = []
         self.__create_cells()
+        self.__break_entrance_and_exit()
+        self.__break_walls_r(0, 0)
 
     def __create_cells(self):
         for i in range(self.__num_cols):
@@ -58,3 +63,41 @@ class Maze:
         last_row = self.__num_rows - 1
         self.__cells[last_col][last_row].has_bottom_wall = False
         self.__draw_cell(last_col, last_row)
+
+    def __break_walls_r(self, i, j):
+        self.__cells[i][j].visited = True
+        while True:
+            neighbors = []
+            # góra
+            if j > 0 and not self.__cells[i][j - 1].visited:
+                neighbors.append(("N", i, j - 1))
+            # dół
+            if j < self.__num_rows - 1 and not self.__cells[i][j + 1].visited:
+                neighbors.append(("S", i, j + 1))
+            # lewo
+            if i > 0 and not self.__cells[i - 1][j].visited:
+                neighbors.append(("W", i - 1, j))
+            # prawo
+            if i < self.__num_cols - 1 and not self.__cells[i + 1][j].visited:
+                neighbors.append(("E", i + 1, j))
+
+            if len(neighbors) == 0:
+                self.__draw_cell(i, j)
+                return
+            
+            direction, ni, nj = random.choice(neighbors)
+
+            if direction == "N":
+                self.__cells[i][j].has_top_wall = False
+                self.__cells[ni][nj].has_bottom_wall = False
+            elif direction == "S":
+                self.__cells[i][j].has_bottom_wall = False
+                self.__cells[ni][nj].has_top_wall = False
+            elif direction == "W":
+                self.__cells[i][j].has_left_wall = False
+                self.__cells[ni][nj].has_right_wall = False
+            elif direction == "E":
+                self.__cells[i][j].has_right_wall = False
+                self.__cells[ni][nj].has_left_wall = False
+
+            self.__break_walls_r(ni, nj)
